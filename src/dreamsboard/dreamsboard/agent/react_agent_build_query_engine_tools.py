c1a65d2b1e46bf7b5f32924d602cc955d0738449
glide-the <2533736852@qq.com>
2024-04-03 18:13:07 +0800

    agent抽取任务

--- src/dreamsboard/dreamsboard/agent/react_agent_build_query_engine_tools.py
+++ src/dreamsboard/dreamsboard/agent/react_agent_build_query_engine_tools.py
@@ -14,99 +14,168 @@ from llama_index.llms.openai import OpenAI
 
 def build_query_docs() -> list[list[Document]]:
     # load data
-    march_docs = SimpleDirectoryReader(
-        input_files=["/home/dmeck/Documents/张毛峰个人简历 - 2024-01-20(1).pdf"]
+
+    # load data
+    meas_docs = SimpleDirectoryReader(
+        input_files=["/home/dmeck/Documents/电网业务中台/电网资源业务中台接口开发规范说明书V2.01-测点管理中心.docx"]
     ).load_data()
-    june_docs = SimpleDirectoryReader(
-        input_files=["/home/dmeck/Downloads/个人简历_刘立兼(1).docx"]
+    topo_docs = SimpleDirectoryReader(
+        input_files=["/home/dmeck/Documents/电网业务中台/电网资源业务中台接口开发规范说明书V2.01-电网拓扑中心.docx"]
     ).load_data()
-    sept_docs = SimpleDirectoryReader(
-        input_files=["/home/dmeck/Downloads/简历_宋金珂_北京交通大学_网络空间安全.pdf"]
+    geo_docs = SimpleDirectoryReader(
+        input_files=["/home/dmeck/Documents/电网业务中台/电网资源业务中台接口开发规范说明书V2.01-电网图形中心.docx"]
     ).load_data()
-    return [march_docs, june_docs, sept_docs]
+    asset_docs = SimpleDirectoryReader(
+        input_files=["/home/dmeck/Documents/电网业务中台/电网资源业务中台接口开发规范说明书V2.01-电网资产中心.docx"]
+    ).load_data()
+    psr_docs = SimpleDirectoryReader(
+        input_files=["/home/dmeck/Documents/电网业务中台/电网资源业务中台接口开发规范说明书V2.01-电网资源中心.docx"]
+    ).load_data()
+    base_docs = SimpleDirectoryReader(
+        input_files=["/home/dmeck/Documents/电网业务中台/电网资源业务中台接口开发规范说明书V2.01-基础服务中心.docx"]
+    ).load_data()
+    return [meas_docs, topo_docs, geo_docs, asset_docs, psr_docs, base_docs]
 
 
 def build_query_docs_index_store(query_docs: list[list[Document]], embeddings: OpenAIEmbedding) -> list[VectorStoreIndex]:
     try:
         storage_context = StorageContext.from_defaults(
-            persist_dir="./storage/marchtest2"
+            persist_dir="./storage/meas"
         )
-        march_index = load_index_from_storage(storage_context)
+        meas_index = load_index_from_storage(storage_context)
         storage_context = StorageContext.from_defaults(
-            persist_dir="./storage/junetest2"
+            persist_dir="./storage/topo"
         )
-        june_index = load_index_from_storage(storage_context)
+        topo_index = load_index_from_storage(storage_context)
 
         storage_context = StorageContext.from_defaults(
-            persist_dir="./storage/septtest2"
+            persist_dir="./storage/geo"
         )
-        sept_index = load_index_from_storage(storage_context)
+        geo_index = load_index_from_storage(storage_context)
+        storage_context = StorageContext.from_defaults(
+            persist_dir="./storage/asset"
+        )
+        asset_index = load_index_from_storage(storage_context)
+        storage_context = StorageContext.from_defaults(
+            persist_dir="./storage/psr"
+        )
+        psr_index = load_index_from_storage(storage_context)
+        storage_context = StorageContext.from_defaults(
+            persist_dir="./storage/base"
+        )
+        base_index = load_index_from_storage(storage_context)
         index_loaded = True
     except:
         index_loaded = False
     if not index_loaded:
         # load data
-        march_docs = query_docs[0]
-        june_docs = query_docs[1]
-        sept_docs = query_docs[2]
+        meas_docs = query_docs[0]
+        topo_docs = query_docs[1]
+        geo_docs = query_docs[2]
+        asset_docs = query_docs[3]
+        psr_docs = query_docs[4]
+        base_docs = query_docs[5]
+
         # build index
-        march_index = VectorStoreIndex.from_documents(
-            march_docs, embed_model=embeddings
+        meas_index = VectorStoreIndex.from_documents(
+            meas_docs,embed_model=embeddings
         )
-        june_index = VectorStoreIndex.from_documents(
-            june_docs, embed_model=embeddings
+        topo_index = VectorStoreIndex.from_documents(
+            topo_docs,embed_model=embeddings
         )
-        sept_index = VectorStoreIndex.from_documents(
-            sept_docs, embed_model=embeddings
+        geo_index = VectorStoreIndex.from_documents(
+            geo_docs,embed_model=embeddings
+        )
+        asset_index = VectorStoreIndex.from_documents(
+            asset_docs,embed_model=embeddings
+        )
+        psr_index = VectorStoreIndex.from_documents(
+            psr_docs,embed_model=embeddings
+        )
+        base_index = VectorStoreIndex.from_documents(
+            base_docs,embed_model=embeddings
         )
 
         # persist index
-        march_index.storage_context.persist(persist_dir="./storage/marchtest2")
+        meas_index.storage_context.persist(persist_dir="./storage/meas")
+        topo_index.storage_context.persist(persist_dir="./storage/topo")
+        geo_index.storage_context.persist(persist_dir="./storage/geo")
+        asset_index.storage_context.persist(persist_dir="./storage/asset")
+        psr_index.storage_context.persist(persist_dir="./storage/psr")
+        base_index.storage_context.persist(persist_dir="./storage/base")
 
-        june_index.storage_context.persist(persist_dir="./storage/junetest2")
-        sept_index.storage_context.persist(persist_dir="./storage/septtest2")
-
-    index_store = [march_index, june_index, sept_index]
+    index_store = [meas_index, topo_index, geo_index, asset_index, psr_index, base_index]
     return index_store
 
 
 def build_query_engine_tools(llm: OpenAI, index_store: list[VectorStoreIndex]) -> list[QueryEngineTool]:
-    march_index = index_store[0]
-    june_index = index_store[1]
-    sept_index = index_store[2]
-    march_engine = march_index.as_query_engine(similarity_top_k=3, llm=llm)
-    june_engine = june_index.as_query_engine(similarity_top_k=3, llm=llm)
-    sept_engine = sept_index.as_query_engine(similarity_top_k=3, llm=llm)
-
-    query_tool_march = QueryEngineTool.from_defaults(
-        query_engine=march_engine,
-        name="resume_zhangmaofeng",
+    meas_index = index_store[0]
+    topo_index = index_store[1]
+    geo_index = index_store[2]
+    asset_index = index_store[3]
+    psr_index = index_store[4]
+    base_index = index_store[5]
+    meas_engine = meas_index.as_query_engine(similarity_top_k=3, llm=llm)
+    topo_engine = topo_index.as_query_engine(similarity_top_k=3, llm=llm)
+    geo_engine = geo_index.as_query_engine(similarity_top_k=3, llm=llm)
+    asset_engine = asset_index.as_query_engine(similarity_top_k=3, llm=llm)
+    psr_engine = psr_index.as_query_engine(similarity_top_k=3, llm=llm)
+    base_engine = base_index.as_query_engine(similarity_top_k=3, llm=llm)
+    query_tool_meas = QueryEngineTool.from_defaults(
+        query_engine=meas_engine,
+        name="meas",
         description=(
-            f"关于张毛峰的简历信息，包括了langchain-chatchat、InterpretationoDreams、KM 平台、省检修特高压生产指挥管控系统、智能运检移动应用、福建监控系统项目"
+            f"关于测点管理中心的业务中台，包括了测量查询服务、事件中心服务、事件类型定义、事件代码表、策略类型、实时开源状态 等业务信息"
             f"eg:检索时带上详细的问题内容"
 
         ),
     )
 
-    query_tool_june = QueryEngineTool.from_defaults(
-        query_engine=june_engine,
-        name="resume_liulijian",
+    query_tool_topo = QueryEngineTool.from_defaults(
+        query_engine=topo_engine,
+        name="topo",
         description=(
-            f"关于刘立兼的简历信息，包括了•篝火心理、雷鸟365、雷鸟365、网聚宝CRM、AP数据基盘、AP数据基盘等项目"
+            f"关于电网拓扑中心的业务中台，包括了一、电网拓扑中心概述、二、拓扑基础分析服务群、三、拓扑高级分析服务 等业务信息"
             f"eg:检索时带上详细的问题内容"
 
         ),
     )
-    query_tool_sept = QueryEngineTool.from_defaults(
-        query_engine=sept_engine,
-        name="resume_songjinke",
+    query_tool_geo= QueryEngineTool.from_defaults(
+        query_engine=geo_engine,
+        name="geo",
         description=(
-            f"关于宋金珂的简历信息，包括了全球 IPv4 空间内的物联网设备扫描识别和隐私安全分、开源软件生态内的跨项目依赖分析及漏洞影响追溯、已发表论文列表、IoT 设备安全等项目"
+            f"关于电网图形中心的业务中台，包括了一、空间查询服务、二、专题图成图服务、三、Gis出图服务、专题图出图服务 等业务信息"
+            f"eg:检索时带上详细的问题内容"
+
+        ),
+    )
+    query_tool_asset = QueryEngineTool.from_defaults(
+        query_engine=asset_engine,
+        name="asset",
+        description=(
+            f"关于电网资产中心的业务中台，包括了资产信息查询服务 业务信息"
             f"eg:检索时带上详细的问题内容"
 
         ),
     )
 
-    query_engine_tools = [query_tool_march, query_tool_june, query_tool_sept]
+    query_tool_psr = QueryEngineTool.from_defaults(
+        query_engine=psr_engine,
+        name="psr",
+        description=(
+            f"关于电网资源中心的业务中台，包括了资源查询服务 业务信息"
+            f"eg:检索时带上详细的问题内容"
 
+        ),
+    )
+    query_tool_base = QueryEngineTool.from_defaults(
+        query_engine=base_engine,
+        name="base",
+        description=(
+            f"关于电基础服务中心的业务中台，包括了3.1电网变更服务、3.2统一认证服务、3.3通用查询服务、3.4结构树服务、3.5权限服务、3.6人员组织查询服务 业务信息"
+            f"eg:检索时带上详细的问题内容"
+
+        ),
+    )
+    query_engine_tools = [query_tool_meas, query_tool_topo, query_tool_geo, query_tool_asset, query_tool_psr, query_tool_base]
     return query_engine_tools
--- src/dreamsboard/dreamsboard/agent/react_agent_finetune_cleaning.py
+++ src/dreamsboard/dreamsboard/agent/react_agent_finetune_cleaning.py
@@ -11,17 +11,8 @@ def adapter_agent(llm_agent: OpenAI, query_engine_tools):
     return base_agent
 
 
-def build_dataset_generator_questions(docs: list[Document], llm: LLM, num=60) -> list[str]:
-    base_question_gen_query = (
-        "你是一名公司人事/HR，你的任务是安排一次简历筛选。"
-        "使用简历提交的文件中提供的上下文， 制定一些问题，"
-        "从上下文中捕捉到重要事实形成问题，"
-        "将问题限制在所提供的上下文信息内."
-        "提取的事实需要验证,不需要标注出具体的来源，例如："
-        "有没有人做了知识库的项目，姓名是谁？"
-        "项目经历中谁负责过项目的调研、设计、实现、测试、维护等全生命周期事宜？"
-        "**请注意不需要标注出问题属于哪些**，例如**个人职业规划**"
-    )
+def build_dataset_generator_questions(base_question_gen_query, docs: list[Document], llm: LLM, num=60) -> list[str]:
+
     # 在
     # /llama_index/core/evaluation/dataset_generation.py:246
     # /llama_index/core/evaluation/dataset_generation.py:274
@@ -38,31 +29,7 @@ def build_dataset_generator_questions(docs: list[Document], llm: LLM, num=60) ->
     return questions
 
 
-def gen_question_variations(generator_questions, llm: LLM, num_vary=3) -> list[str]:
-    vary_question_tmpl = """\
-你是一位公司HR。给定一个关于招聘的问题，你的目标是生成多达 {num_vary} 个问题变体，涉及多个求职者简历。
-
-这可能包括比较/对比不同求职者简历，用另一个简历替换当前的简历，或生成只能通过多个关于简历回答的问题（发挥创意！）
-
-你被提供了一组有效的求职者简历。请仅生成可以在该组简历中回答的问题变体。
-
-For example:
-Base Question: "哪位求职者拥有最多的项目管理经验？"
-Valid 10Qs: [《关于张三的简历信息》, 《关于李四的简历信息》, 《关于王五的简历信息》]
-Question Variations:
-在张三和李四之间，谁有更多的领导经验？
-如果我想找一个有强大数据分析技能的求职者，我应该选择哪个简历？
-对比王五和张三的简历，哪个更适合市场营销职位？
-基于现有简历，谁显示出最强的团队合作能力？
-
-现在让我们试试吧！
-
-Base Question: {base_question}
-Valid 10Qs: {valid_10qs}
-Question Variations:
-"""
-
-    VALID_10Q_STR = "[关于张毛峰的简历信息, 关于刘立兼的简历信息, 关于宋金珂的简历信息]"
+def gen_question_variations(vary_question_tmpl, valid_10q_str, generator_questions, llm: LLM, num_vary=3) -> list[str]:
 
     prompt_tmpl = PromptTemplate(vary_question_tmpl)
 
@@ -73,7 +40,7 @@ Question Variations:
             prompt_tmpl.format(
                 num_vary=num_vary,
                 base_question=question,
-                valid_10qs=VALID_10Q_STR,
+                valid_10qs=valid_10q_str,
             )
         )
         # parse into newlines
