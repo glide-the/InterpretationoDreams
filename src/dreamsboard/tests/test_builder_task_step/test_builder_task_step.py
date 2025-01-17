d0a51dd38c8b342c7b6a8640724470a260c9b72c
glide-the <dmeck@suoxya.com>
2025-01-17 17:36:41 +0800

    增加多线程

--- src/dreamsboard/tests/test_builder_task_step/test_builder_task_step.py
+++ src/dreamsboard/tests/test_builder_task_step/test_builder_task_step.py
@@ -9,6 +9,8 @@ from dreamsboard.engine.memory.mctsr.prompt import RefineResponse
 from dreamsboard.dreams.task_step_md.base import TaskStepMD
 from dreamsboard.common import _get_assistants_tool
 
+from dreamsboard.engine.storage.task_step_store.types import BaseTaskStepStore
+from dreamsboard.engine.task_engine_builder.core import TaskEngineBuilder
 import logging
 import os
 from dreamsboard.dreams.task_step_to_question_chain.weaviate.prepare_load import get_query_hash
@@ -283,3 +285,114 @@ def test_task_step_md():
     task_step_md = TaskStepMD(task_step_store)
     md_text =   task_step_md.format_md()
     print(md_text.text)
+
+
+
+def test_builder_task_step_mctsr_threads():
+    import threading
+
+    llm = ChatOpenAI(
+        openai_api_base=os.environ.get("API_BASE"),
+        model=os.environ.get("API_MODEL"),
+        openai_api_key=os.environ.get("API_KEY"),
+        verbose=True,
+        temperature=0.1,
+        top_p=0.9,
+    )
+    kor_dreams_task_step_llm = ChatOpenAI(
+        openai_api_base=os.environ.get("API_BASE"),
+        model=os.environ.get("API_MODEL"),
+        openai_api_key=os.environ.get("API_KEY"),
+        verbose=True,
+        temperature=0.95,
+        top_p=0.70,
+    )
+
+    if 'glm' in os.environ.get("API_MODEL"):
+
+        tools= [ { "type": "web_search",   "web_search": {"enable": False ,"search_result": False   }}]
+    else:
+        tools = []
+    llm_with_tools = llm.bind(   tools=[_get_assistants_tool(tool) for tool in tools] )
+    kor_dreams_task_step_llm_with_tools = kor_dreams_task_step_llm.bind(   tools=[_get_assistants_tool(tool) for tool in tools] )
+
+    from tests.test_builder_task_step.prompts import (
+        AEMO_REPRESENTATION_PROMPT_TEMPLATE as AEMO_REPRESENTATION_PROMPT_TEMPLATE_TEST,
+        STORY_BOARD_SCENE_TEMPLATE as STORY_BOARD_SCENE_TEMPLATE_TEST,
+        STORY_BOARD_SUMMARY_CONTEXT_TEMPLATE as STORY_BOARD_SUMMARY_CONTEXT_TEMPLATE_TEST,
+        EDREAMS_EVOLUTIONARY_TEMPLATE as EDREAMS_EVOLUTIONARY_TEMPLATE_TEST,
+        EDREAMS_PERSONALITY_TEMPLATE as EDREAMS_PERSONALITY_TEMPLATE_TEST,
+        DREAMS_GEN_TEMPLATE as DREAMS_GEN_TEMPLATE_TEST,
+    ) 
+    os.environ["AEMO_REPRESENTATION_PROMPT_TEMPLATE"] = AEMO_REPRESENTATION_PROMPT_TEMPLATE_TEST
+    os.environ["STORY_BOARD_SCENE_TEMPLATE"] = STORY_BOARD_SCENE_TEMPLATE_TEST
+    os.environ["STORY_BOARD_SUMMARY_CONTEXT_TEMPLATE"] = STORY_BOARD_SUMMARY_CONTEXT_TEMPLATE_TEST
+    os.environ["EDREAMS_EVOLUTIONARY_TEMPLATE"] = EDREAMS_EVOLUTIONARY_TEMPLATE_TEST
+    os.environ["EDREAMS_PERSONALITY_TEMPLATE"] = EDREAMS_PERSONALITY_TEMPLATE_TEST
+    os.environ["DREAMS_GEN_TEMPLATE"] = DREAMS_GEN_TEMPLATE_TEST
+
+
+    # 存储
+    cross_encoder_path = "/mnt/ceph/develop/jiawei/model_checkpoint/jina-reranker-v2-base-multilingual"
+    embed_model_path = "/mnt/ceph/develop/jiawei/model_checkpoint/m3e-base"
+    start_task_context = "什么是损失函数？"
+    builder = StructuredTaskStepStoryboard.form_builder(
+        llm_runable=llm_with_tools,
+        kor_dreams_task_step_llm=kor_dreams_task_step_llm_with_tools,
+        start_task_context=start_task_context,
+        cross_encoder_path=cross_encoder_path,
+        embed_model_path=embed_model_path
+    )
+    # 初始化任务引擎
+    task_engine_builder = builder.loader_task_step_iter_builder(allow_init=False)
+
+    def worker(task_engine: TaskEngineBuilder, task_step_store: BaseTaskStepStore):
+        owner = f"thread {threading.get_native_id()}"
+        logger.info(f"{owner}，任务开始")
+        if not task_engine.check_engine_init():
+            task_engine.init_task_engine()
+            task_engine.init_task_engine_dreams()
+            task_engine.init_task_engine_storyboard_executor()
+
+        try:
+            code_gen_builder = task_engine.storyboard_code_gen_builder()
+            task_step = task_engine.task_step_store.get_task_step(task_engine.task_step_id)
+            if task_step.task_step_question_answer is None or len(task_step.task_step_question_answer) == 0:
+                task_engine.generate_step_answer(code_gen_builder)
+            mcts_node = task_engine.get_mcts_node()
+            answer = mcts_node.run()
+            
+            mcts_node.print()
+            print(answer)
+            task_step.task_step_question_answer = answer 
+            task_step_id = task_engine.task_step_id
+            
+            task_engine.task_step_store.add_task_step([task_step])
+            task_step_store_path = concat_dirs(dirname=f"{builder.base_path}/storage/{task_step_id}", basename=DEFAULT_PERSIST_FNAME)
+            task_engine.task_step_store.persist(persist_path=task_step_store_path) 
+            
+            task_step_store.add_task_step([task_step])
+            task_step_store_path = concat_dirs(dirname=f"{builder.base_path}/storage", basename=DEFAULT_PERSIST_FNAME)
+            task_step_store.persist(persist_path=task_step_store_path) 
+ 
+        except Exception as e:
+            logger.error("场景加载失败", e)
+ 
+        logger.info(f"{owner}，任务结束")
+
+    threads = []
+    
+    # step =0
+    task_step_store = builder.task_step_store
+    while not task_engine_builder.empty():
+       
+        task_engine = task_engine_builder.get()
+ 
+        t = threading.Thread(target=worker,
+                             kwargs={"task_engine": task_engine, "task_step_store": task_step_store},
+                             daemon=True)
+        t.start()
+        threads.append(t)
+
+    for t in threads:
+        t.join()
