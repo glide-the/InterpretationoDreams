8f2a84481c8bbeb5f98380e5d390f6bd37df2a3d
glide-the <dmeck@suoxya.com>
2025-01-23 18:46:36 +0800

    增加一些示例

--- src/dreamsboard/dreamsboard/dreams/task_step_md/prompts.py
+++ src/dreamsboard/dreamsboard/dreams/task_step_md/prompts.py
@@ -12,12 +12,12 @@ TASK_MD_TEMPLATE = """
 TASK_STEP_MD_TITLE_TEMPLATE = """
 {task_step_name} [task_id:{task_step_level}]({task_step_id})
 
-{task_step_description}
+{task_step_question_answer}
 """
 
 
 TASK_STEP_MD_DESC_TEMPLATE = """
-{task_step_name} [task_id:{task_step_level}]({task_step_id}){task_step_description} {task_step_question_answer}
+{task_step_name} [task_id:{task_step_level}]({task_step_id}) {task_step_question_answer}
 """
 
 
