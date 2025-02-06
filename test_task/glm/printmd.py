126d593575d995b718128bf65e71f30418620abb
glide-the <dmeck@suoxya.com>
2025-02-06 15:20:28 +0800

    1

--- test_task/glm/printmd.py
+++ test_task/glm/printmd.py
@@ -7,8 +7,8 @@ from dreamsboard.dreams.task_step_to_question_chain.weaviate.prepare_load import
 
 
 if __name__ == "__main__":
-    
-    start_task_context = "MCTS在PRM偏好策略模型微调的应用探索综述"
+        
+    start_task_context = os.environ.get("start_task_context")
     base_path = f'./{get_query_hash(start_task_context)}/'
     task_step_store = SimpleTaskStepStore.from_persist_dir(f'./{base_path}/storage')
     task_step_md = TaskStepMD(task_step_store)
